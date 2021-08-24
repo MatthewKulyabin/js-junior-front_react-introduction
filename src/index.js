a797e830370bc76697e1831b4cd3d5e730f1512a
MrEnderNasha <mc.mine.production@gmail.com>
2021-08-24 11:21:42 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const element = <h1>Hello World меня зовут: Матфей Кулябин</h1>;
+ReactDom.render(element, document.querySelector('#root'));
